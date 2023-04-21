6171466837304879478a1ec8a737320e009cf19c
ShantanuWelling <shantanuwelling@gmail.com>
2023-04-21 21:04:30 +0530

    update 21.04 2100 hrs

---
+++ README.md
@@ -0,0 +1,28 @@
+Topic 3. Implement FCM clustering algorithm and evaluate the cluster quality for different values of ‘C’. (Minimum and maximum values of C to be specified by user) 
+
+Team Members: Shantanu Welling (210010076), Harshit Agarwal (210020054), Arijit Saha (210050017)
+​
+Requirements: Python libraries- numpy, matplotlib, scikit-learn, scikit-image, scipy, PIL (Pillow), sys
+
+Run executable python script named fcm.py from Linux terminal as:
+python3 fcm.py {input_image_name} {min_clusters} {max_clusters}
+
+The input image name must be specified as a path relative to the directory in which the script fcm.py is placed in.
+
+Prints optimal number of clusters along with the corresponding Davies Bouldin Score as "Optimal Number of clusters = 6 with Davies Bouldin score of 0.49954825815125287" on the terminal. 
+
+Also saves (in the same directory as that of the script) the clustering images for each value of C as "output_C{i}.jpeg" where i is the number of clusters used for clustering the image. 
+
+It also saves the original image and the optimal clustered image (side by side in the same image) in the file named "Original_BestCluster.jpeg" in the same directory as that of the script.
+
+Also saves the corresponding graph for the input image which plots Number of clusters vs Davies Bouldin score values for the corresponding clustering in a file named "CQ_plot.png" in the same directory as that of the script.
+
+Folder named "results" in this zip file contains subfolders named "img{i}" where i corresponds to the input file named "img{i}.png"/"img{i}.jpeg" that was used. There are 15 images inside each subfolder.
+The 15 images in subfolder "img{i}" correspond to:
+13 clustering images obtained (output_C{i}.jpeg where i ranges from 3 to 15- both inclusive)
+1 Graph plot (CQ_plot.png)
+1 Original vs Best Clustered image (Original_BestCluster.jpeg)
+These 15 images of subfolder "img{i}" are obtained after executing the following command on terminal:
+"python3 fcm.py img{i}.png 3 15" or "python3 fcm.py img{i}.jpeg 3 15" depending on whether the img is in jpeg or png data format. Each input image has range of cluster values between 3 and 15 (both inclusive) as the user input parameters.
+There are 8 such subfolders inside "results" folder corresponding to the results obtained from the 8 input images in the zip file's main (parent) directory (img{i} where i ranges from 1 to 8-both inclusive) (having .png/.jpeg extension)
+There is a res.txt file in the main (parent) directory which has the Optimal Cluster number along with the corresponding Davies Bouldin Score for each of the 8 input images used. (i.e. the output printed on terminal for each input image).
--- fcm.py
+++ fcm.py
@@ -2,14 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import data, color, io
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import MinMaxScaler
 from scipy.spatial.distance import cdist
-from sklearn.metrics import calinski_harabasz_score
 from sklearn.metrics import davies_bouldin_score
 from PIL import Image
 import sys
-
+#Import relevant libraries
 
 def fcm(X, C, m, max_iter=100, error=1e-5, init=None):
     # Initialize membership matrix using K-means algorithm
@@ -51,7 +49,7 @@ if sys.version_info.major == 3:
     python_command = "python3"
 else:
     python_command = "python"
-
+#Check Sys args
 if(len(sys.argv)!=4):
     print(f"Error: Usage {python_command} {sys.argv[0]} input_image_name C_min C_max")
     exit(1)
@@ -86,39 +84,47 @@ for k in Clusters:
     error = 1e-5  # Convergence threshold
     centroids, U = fcm(X, C, m, max_iter, error, init='kmeans')
 
-    # Compute Silhouette score
+    # Compute Davies Bouldin score
     labels = np.argmax(U, axis=1)
     Labels.append(labels)
-    # score = silhouette_score(X, labels)
-    # score = calinski_harabasz_score(X, labels)
     score = davies_bouldin_score(X, labels)
     scores.append(score)
     # Reshape the labels and centroids back to the original image shape
     labels = labels.reshape(image.shape)
     centroids = centroids.reshape((C,))
-
+    #plot and save the clustered image
     plt.imshow(labels, cmap='gray')
-    plt.title(f'Segmented image (C={C}, score={score:.4f})')
+    plt.title(f'Clustered image (C={C}, score={score:.4f})')
     for c in centroids:
         plt.axhline(c, color='white')
     outfileName = 'output_C' + str(k) + ".jpeg"
     outputs.append(outfileName)
-    plt.savefig(outfileName)
+    plt.savefig(outfileName, dpi=300)
     print(f"Iteration corresponding to C={k} done")
+    plt.close()
 
-MaxScore = max(scores)
-index_maxScore = scores.index(MaxScore)
-C_max = Clusters[index_maxScore]
-output = outputs[index_maxScore]
-label = Labels[index_maxScore]
-label = label.reshape(image.shape)
+MinScore = min(scores) #Minimum Davies Boulding Score
+index_minScore = scores.index(MinScore) #Corresponding index of MinScore
+C_opt = Clusters[index_minScore] #Optimal C value which corresponds to the minimum Davies Boulding Score
+output = outputs[index_minScore] #Output file corresponding to the index
+label = Labels[index_minScore]  #Labelled image corresponding to the index
+label = label.reshape(image.shape) #Reshape image of labels to the original image's shape
 # Read image
 img = Image.open(output)
  
+#Plot the graph for Cluster Quality Evaluation
+plt.plot(Clusters,scores,'b--')
+plt.plot(Clusters,scores,'bo')
+plt.xlabel("Number of Clusters")
+plt.ylabel("Davies Bouldin Score")
+plt.title("Cluster Quality Evaluation")
+plt.ylim(0.99*min(scores),1.01*max(scores))
+plt.savefig("CQ_plot.png")
+plt.close()
 # Output Images
 img.show()
-
-print(f"Max Score corresponds to image with Number of clusters = {C_max} with score of {MaxScore}")
+print(f"Optimal Number of clusters = {C_opt} with Davies Bouldin score of {MinScore}")
+img.close() #EDITED SW
 #Plot the segmented image
 fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(10, 5))
 
@@ -126,9 +132,7 @@ ax0.imshow(image1, cmap='gray')
 ax0.set_title('Original image')
 
 ax1.imshow(label, cmap='gray')
-ax1.set_title(f"Segmented Image for C = {C_max} with Max Score")
+ax1.set_title(f"Clustered Image for C = {C_opt} with Best Score of {MinScore:.4f}")
 
-plt.savefig("Original_BestSegmented.jpeg")
-
-img = Image.open("Original_BestSegmented.jpeg")
-img.show()
+plt.savefig("Original_BestCluster.jpeg", dpi=300)
+plt.close() #EDITED SW
